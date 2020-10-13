3dc8b0e65ac72b5c28dac9511361f19cccf03265
Your Name <you@example.com>
2020-10-13 14:55:41 +0300

    make http request with standard lib, to avoid wasting time installing requests-html with pip on every run

--- yoomoney_exchange_rates.py
+++ yoomoney_exchange_rates.py
@@ -1,21 +1,15 @@
-from requests_html import HTMLSession
+import urllib.request
 import re
 import json
 import sys
 
 def main():
-    session = HTMLSession()
-    r = session.get("https://money.yandex.ru/account/exchange-rates")
-    # json containing exchange rates is inlined in script element on the page,
-    # so look only for elements without src attribute
-    scripts = filter(lambda element: "src" not in element.attrs, r.html.find("script"))
-    for s in scripts:
-        # find js objects: {"currencyCode": "USD", ...}
-        objects = re.findall("({[^{]+?})", s.full_text)
-        # there are many js objects, we only need those with currency exhcnage rates
-        objects = list(filter(lambda o: "currencyCode" in o and "buyRate" in o, objects))
-        if objects:
-            break
+    with urllib.request.urlopen("https://money.yandex.ru/account/exchange-rates") as f:
+        text = f.read().decode("utf8")
+    # find possible js objects: {"currencyCode": "USD", ...}
+    objects = re.findall("({[^{]+?})", text)
+    # there are many js objects, we only need those with currency exhcnage rates
+    objects = list(filter(lambda o: "currencyCode" in o and "buyRate" in o, objects))
     if not objects:
         sys.exit(1)
     print(f"{'CURRENCY':<10} {'BUYRATE':<10} {'SELLRATE':<10}")
